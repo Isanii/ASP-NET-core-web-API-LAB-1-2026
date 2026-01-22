1b9aef5884448884702dceb83cf9986302ae65ec
Isancodez <qtgamer1612@gmail.com>
2026-01-22 19:52:01 +0700

    Add project files.

---
+++ StudentAPI/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
